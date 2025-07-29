9e0cca76ee5bc95b0145a7d0569facf90f1ec86f
Amruta Velamuri <amvelamuri@gmail.com>
2025-07-29 12:09:09 -0500

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Amruta Velamuri on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapstoneProjectTests/CapstoneProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectTests.swift
+//  CapstoneProjectTests
+//
+//  Created by Amruta Velamuri on 7/29/25.
+//
+
+import Testing
+@testable import CapstoneProject
+
+struct CapstoneProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

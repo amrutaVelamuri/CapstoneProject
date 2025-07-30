1540b660175ea75528b54e1ab856ca0c0e729e7a
Aishani <aishaniv30@gmail.com>
2025-07-30 12:36:50 -0500

    To-Do List!!!

---
+++ CapstoneProject/ToDoList.swift
@@ -0,0 +1,62 @@
+//
+//  ToDoList.swift
+//  CapstoneProject
+//
+//  Created by Ranjan Verma on 7/29/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ToDoList: View {
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
+    var body: some View {
+        VStack {
+            HStack {
+                Text("To Do List")
+                    .font(.system(size:40))
+                    .fontWeight(.black)
+                Spacer()
+                Button {
+                    withAnimation {
+                        showNewTask = true
+                    }
+                } label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+            }
+        }
+        .padding()
+        Spacer()
+        List {
+            ForEach(toDos) { toDoItem in
+                if toDoItem.isImportant {
+                    Text("‼️" + toDoItem.title)
+                } else {
+                    Text(toDoItem.title)
+                }
+            }
+            .onDelete(perform: deleteToDo)
+        }
+        .listStyle(.plain)
+        
+        if showNewTask {
+            NewToDo(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+        }
+    }
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
+        }
+    }
+}
+
+#Preview {
+    ToDoList()
+        .modelContainer(for: ToDoItem.self, inMemory: true)
+}
